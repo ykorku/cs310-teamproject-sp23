983c9eefecba6618384f7daa0036b0ac82251c2a
Dalton Estes <destes2@stu.jsu.edu>
2023-02-22 13:19:41 -0600

    Testing department branch push

---
+++ src/edu/jsu/mcis/cs310/tas_sp23/Department.java
@@ -0,0 +1,13 @@
+package edu.jsu.mcis.cs310.tas_sp23;
+
+public class Department {
+    
+    private final String id, description, terminalid;
+    
+    public Department(String id, String description, String terminalid){
+        this.id = id;
+        this.description = description;
+        this.terminalid = terminalid;
+    }
+    
+}

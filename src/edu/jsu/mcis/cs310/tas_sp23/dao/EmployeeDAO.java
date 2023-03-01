e350981d462fb7ce91659353bf666b8cf41148a1
Dalton Estes <destes2@stu.jsu.edu>
2023-02-28 19:05:55 -0600

    test file not passing

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/EmployeeDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/EmployeeDAO.java
@@ -5,8 +5,11 @@ import edu.jsu.mcis.cs310.tas_sp23.Badge;
 import edu.jsu.mcis.cs310.tas_sp23.Shift;
 import edu.jsu.mcis.cs310.tas_sp23.Punch;
 import edu.jsu.mcis.cs310.tas_sp23.Employee;
+import edu.jsu.mcis.cs310.tas_sp23.EmployeeType;
+import edu.jsu.mcis.cs310.tas_sp23.EventType;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 
 public class EmployeeDAO {
     
@@ -42,7 +45,35 @@ public class EmployeeDAO {
 
                     while (rs.next()) {
                         
+                        EmployeeDAO employeeDAO = new EmployeeDAO(daoFactory);
+                        BadgeDAO badgeDAO = new BadgeDAO(daoFactory);
+                        DepartmentDAO departmentDAO = new DepartmentDAO(daoFactory);
+                        ShiftDAO shiftDAO = new ShiftDAO(daoFactory);
+                        
+                        String badgeId = rs.getString("badgeid");
+                        Badge badge = badgeDAO.find(badgeId);
+                        Department department = departmentDAO.find(id);
+                        Shift shift = shiftDAO.find(id);
+                        
+                        String firstname = rs.getString("firstname");
+                        String middlename = rs.getString("middle");
+                        String lastname = rs.getString("lastname");
+                        
+                        //Get eventtype. 
+                        
+                        int employeetype = rs.getInt("employeetypeid");
+                        EmployeeType employeetypeid = EmployeeType.values()[employeetype];
+  
+                        //Get timestamp from database. It must be casted into LocalDateTime. 
+                        
+                        java.sql.Timestamp timestamp = new Timestamp(new java.util.Date().getTime());
+                        timestamp = rs.getTimestamp("active");
+                        LocalDateTime active = timestamp.toLocalDateTime();  
 
+                        //create employee variable.
+                        
+                        employee = new Employee(id, firstname, lastname, middlename, active, badge, department, shift, employeetypeid);
+                        
                     }
 
                 }

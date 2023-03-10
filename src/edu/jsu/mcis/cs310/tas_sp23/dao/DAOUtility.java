2b7971fcb48104099bf3a3b8709e67da35759ef7
Yunus Korku <ykorku@stu.jsu.edu>
2023-03-10 12:41:55 -0600

    not completed

--- src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
+++ src/edu/jsu/mcis/cs310/tas_sp23/dao/DAOUtility.java
@@ -2,7 +2,6 @@ package edu.jsu.mcis.cs310.tas_sp23.dao;
 
 import java.time.*;
 import java.util.*;
-import java.time.temporal.ChronoUnit;
 import edu.jsu.mcis.cs310.tas_sp23.EventType;
 import edu.jsu.mcis.cs310.tas_sp23.Punch;
 import edu.jsu.mcis.cs310.tas_sp23.Shift;
@@ -14,6 +13,85 @@ import edu.jsu.mcis.cs310.tas_sp23.Shift;
  * 
  */
 public final class DAOUtility {
-       
+        public static int calculateTotalMinutes(ArrayList<Punch> dailypunchlist, Shift shift) {
+            
+            int totalMinutes = 0;
+            LocalTime in = null ;
+            LocalTime out = null ;
+            
+            LocalTime lunchin=shift.getLunchstart();
+            LocalTime lunchout=shift.getLunchstop();
+            
+            boolean lunchused=false;
+           
+            
+
+            for (int i = 0; i < dailypunchlist.size(); i++) {
+                
+                
+                
+                Punch punch = dailypunchlist.get(i);
+                EventType punchtype = punch.getPunchtype();
+                
+                
+
+                if (punchtype == EventType.CLOCK_IN) {
+                    LocalTime dateFromDateTime=punch.getAdjustedtimestamp().toLocalTime();
+                    
+                    in = dateFromDateTime;
+                } 
+                else if (punchtype == EventType.CLOCK_OUT ) {
+                    LocalTime dateFromDateTime=punch.getAdjustedtimestamp().toLocalTime();
+                    
+                    out = dateFromDateTime;
+                } 
+                else if (punchtype == EventType.TIME_OUT) {
+                    Punch prevpunch = dailypunchlist.get(i-1);
+                    EventType prevpunchtype = prevpunch.getPunchtype();
+                    if(punchtype == EventType.CLOCK_IN && prevpunchtype == EventType.TIME_OUT){
+                        in = null;
+                        out = null;
+                    }
+                } 
+                
+                if(in.isAfter(lunchin)&&in.isBefore(lunchout)){
+                        lunchused=true;
+                    }
+                    if(out.isAfter(lunchin) && out.isBefore(lunchout)){
+                        lunchused=true;
+                    }
+                    if(in.equals(lunchin) && in.equals(lunchout)){
+                        lunchused=true;
+                    }
+                    if(out.equals(lunchin) && out.equals(lunchout)){
+                        lunchused=true;
+                    }
+                
+
+                if (out != null) {
+                    Duration duration = Duration.between(in, out);
+                    long minutesBetween = duration.toMinutes();
+                    
+                    totalMinutes += minutesBetween;
+                    
+                    in = null;
+                    out = null;
+                    
+                } 
+                
+                if(lunchused==false&&totalMinutes == 360 ||totalMinutes>360){
+                        
+                    totalMinutes-=30;
+                    lunchused=true;
+                        
+                }
+                    
+            }
+            
+            
+            
+            return totalMinutes;
+        }
+
 
 }
